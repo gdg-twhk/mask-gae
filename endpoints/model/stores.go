ef35413fe4c5df0c7c5534a0ede8299cc8bb21e3
Kai-Chu Chung <cage.chung@gmail.com>
2020-02-09 08:29:22 +0800

    feat(psql): add custom_note, website properties
    psql according to JSON source add custom_note, website
    
    this is resolved #5

--- endpoints/model/stores.go
+++ endpoints/model/stores.go
@@ -6,18 +6,20 @@ import (
 )
 
 type Store struct {
-	Id        string    `json:"id" db:"id"`
-	Distance  float64   `json:"distance" db:"distance"`
-	Name      string    `json:"name" db:"name"`
-	Phone     string    `json:"phone" db:"phone"`
-	Address   string    `json:"address" db:"address"`
-	MaskAdult uint64    `json:"maskAdult" db:"mask_adult"`
-	MaskChild uint64    `json:"maskChild" db:"mask_child"`
-	Updated   time.Time `json:"updated" db:"updated"`
-	Available string    `json:"available" db:"available"`
-	Note      string    `json:"note" db:"note"`
-	Longitude float64   `json:"longitude" db:"longitude"`
-	Latitude  float64   `json:"latitude" db:"latitude"`
+	Id         string    `json:"id" db:"id"`
+	Distance   float64   `json:"distance" db:"distance"`
+	Name       string    `json:"name" db:"name"`
+	Phone      string    `json:"phone" db:"phone"`
+	Address    string    `json:"address" db:"address"`
+	MaskAdult  uint64    `json:"maskAdult" db:"mask_adult"`
+	MaskChild  uint64    `json:"maskChild" db:"mask_child"`
+	Updated    time.Time `json:"updated" db:"updated"`
+	Available  string    `json:"available" db:"available"`
+	CustomNote string    `json:"custom_note" db:"custom_note"`
+	Website    string    `json:"website" db:"website"`
+	Note       string    `json:"note" db:"note"`
+	Longitude  float64   `json:"longitude" db:"longitude"`
+	Latitude   float64   `json:"latitude" db:"latitude"`
 }
 
 func (p *Store) MarshalJSON() ([]byte, error) {
